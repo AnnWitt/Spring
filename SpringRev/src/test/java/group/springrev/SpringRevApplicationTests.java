76fe81b0e696cca6a63e75f01a6b7ecaceff0399
Anna Wittmann <annwitt@tlen.pl>
2022-02-19 09:59:50 +0100

    Demeter - initial commit

---
+++ SpringRev/src/test/java/group/springrev/SpringRevApplicationTests.java
@@ -0,0 +1,13 @@
+package group.springrev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ demeter/src/test/java/annwitt/com/demeter/DemeterApplicationTests.java
@@ -0,0 +1,13 @@
+package annwitt.com.demeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
